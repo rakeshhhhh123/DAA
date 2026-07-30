795fd47aae240b48a87d53871c8c2f2456f55de8
Rakesh <majjarirakesh4@gmail.com>
2026-07-31 01:02:07 +0530

    Implement selection sort algorithm in selectionsort.py

---
+++ selectionsort.py
@@ -0,0 +1,10 @@
+numbers = [8, 3, 6, 1, 5]
+
+for i in range(len(numbers)):
+    min_index = i
+    for j in range(i + 1, len(numbers)):
+        if numbers[j] < numbers[min_index]:
+            min_index = j
+    numbers[i], numbers[min_index] = numbers[min_index], numbers[i]
+
+print(numbers)
