9231c7a5613e45bdb0203bb6ac9a908059d7da43
Rakesh <majjarirakesh4@gmail.com>
2026-08-13 21:25:26 +0530

    Modify insertion sort to accept user input

--- insertionsort.py
+++ insertionsort.py
@@ -1,13 +1,18 @@
-numbers = [7, 2, 9, 4, 1]
+n = int(input("Enter number of elements: "))
 
-for i in range(1, len(numbers)):
-    key = numbers[i]
+a = []
+
+for i in range(n):
+    a.append(int(input("Enter element: ")))
+
+for i in range(1, n):
+    key = a[i]
     j = i - 1
 
-    while j >= 0 and numbers[j] > key:
-        numbers[j + 1] = numbers[j]
-        j -= 1
+    while j >= 0 and a[j] > key:
+        a[j + 1] = a[j]
+        j = j - 1
 
-    numbers[j + 1] = key
+    a[j + 1] = key
 
-print(numbers)
+print("Sorted list:", a)
